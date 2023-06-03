f71a213f50059ad4007063c49cc75a1ba4c2d8f6
Veinix <work.davidaviles@gmail.com>
2023-06-04 01:21:55 +0300

    Big change to code

--- main.js
+++ main.js
@@ -1,138 +1,138 @@
 "use strict";
 
-// Using querySelector method to get the elements for the input fields and their values, the section we will put all the "sticky notes" in and the reset/save buttons.
-const saveButton = document.querySelector("#saveButton");
-const resetButton = document.querySelector("#resetButton");
-const storageResetButton = document.querySelector("#storageResetButton");
+//Validation handler
+function validationHandler() {
+    event.preventDefault();
+    //checkValidity returns true if the form is valid
+    if (inputForm.checkValidity()) addData(); 
+    else inputForm.reportValidity();
+}
 
-//Load data on page load and display all
-loadData();
+//Update the minDate every minute
+setInterval(setMinDate, 60_000);
 
-//#Validation handlers
-//Validating date and time seperately since their minimum changes dynamically
-//Checks for inputs to the inputform
-const inputForm = document.querySelector("#inputForm");
-inputForm.addEventListener("input", () => {
-    validateDateInput();
-    validateHourInput();
-});
-
-// Using an event listener to look for a submit in the form.
-inputForm.addEventListener("submit", () => {
-    event.preventDefault(); // Prevents page reload
-    if (inputForm.checkValidity()) {
-    addData(); // Call the addData function if the form is valid
-    } else {
-    inputForm.reportValidity(); // Form is invalid, displays error messages.
-    }
-})
+//Sets the min property of the date input
+function setMinDate() {
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+    let month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    let day =  String(currentDate.getDate()).padStart(2, '0');
+    let hours =  String(currentDate.getHours()).padStart(2, '0');
+    let minutes =  String(currentDate.getMinutes()).padStart(2, '0');
+    let formattedDateTime = `${year}-${month}-${day}T${hours}:${minutes}`;
+    
+    const taskDeadlineDateBox = document.querySelector("#taskDeadlineDateBox");
+    taskDeadlineDateBox.min = formattedDateTime;
+}
 
 //Reset button to remove all the notes from storage and refresh the page
-resetButton.addEventListener("click", () => { 
+function resetNotes(){
     localStorage.clear();
     location.reload();
-})
+}
 
 function addData() {
-    //Getting HTML Elements
+    // Getting HTML Elements
     const taskDetailsBox = document.querySelector("#taskDetailsBox");
     const taskDeadlineDateBox = document.querySelector("#taskDeadlineDateBox");
-    const taskDeadlineHourBox = document.querySelector("#taskDeadlineHourBox");
 
-   
-    //Getting values
+    // Getting values
     let taskDetailsValue = taskDetailsBox.value
     let taskDeadlineDateValue = taskDeadlineDateBox.value
-    let taskDeadlineHourValue = taskDeadlineHourBox.value
 
-    //Changing the date format
-    let dateArray = taskDeadlineDateValue.split("-");
-    taskDeadlineDateValue = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`
+    // Changing the date format
+    let hourArray = taskDeadlineDateValue.split("T")[1];
+    let dateArray = taskDeadlineDateValue.split("T")[0];
+    let newDateArray = dateArray.split("-");
+    taskDeadlineDateValue = `${newDateArray[2]}-${newDateArray[1]}-${newDateArray[0]} ${hourArray}`;
 
-    //#Load old data from localStorage
+    // Load old data from localStorage
     let storedNotes = localStorage.getItem("stickyNotes");
     let stickyNotesArray = [];
     if (storedNotes) stickyNotesArray = JSON.parse(storedNotes);
 
-    //#Adding new note
-    // Pushing the new values (single note array) into the array, converting to string, then storing
-    let singleNote = [taskDetailsValue,taskDeadlineDateValue,taskDeadlineHourValue];
+    // Adding new note
+    // Pushing the new values (single note array) into the array, converting to string, then storing. Adding boolean at the end to determine if it has been displayed already (Will be used to determine animation)
+    let singleNote = [taskDetailsValue, taskDeadlineDateValue, false];
     stickyNotesArray.push(singleNote);
     let stickyNotesJSON = JSON.stringify(stickyNotesArray);
     localStorage.setItem("stickyNotes", stickyNotesJSON);
 
-    //# Load updated data
-    //In order for the animation to occur per div, we will manipulate the DOM every time there is a new task. Below is how that is done.
-    const stickyNoteArea = document.querySelector("#stickyNoteArea");
-    //Creating the stickyNoteDiv with the aforementioned class. 
-    const newStickyNoteDiv = document.createElement("div"); 
-    newStickyNoteDiv.setAttribute("class", "stickyNoteDiv");
-    newStickyNoteDiv.innerHTML = 
-    `
-    <i class="bi bi-x-square"></i>
-    <img src="/assets/stickyNote.png" class="stickyNoteImg">
-    <p class="stickyNoteText"> ${singleNote[0]}</p>
-    <span class="stickyNoteDate">${singleNote[1]} ${singleNote[2]}</span>
-    `;
-    //Appending the newly created sticky note to the end of the the sticky note area
-    stickyNoteArea.appendChild(newStickyNoteDiv);
+    // Load updated data
+    loadData();
 
     //Clearing the fields after submit
     document.querySelector("#inputForm").reset();
+    
 }
 
 function loadData() {
-    const stickyNoteArea = document.querySelector("#stickyNoteArea");
     //Assigning the stored data to storedNotes variable
     let storedNotes = localStorage.getItem("stickyNotes"); 
+
     //Checking if local storage (storedNotes) is empty. If it is, does not update.
     if (!storedNotes) return;
 
     //Parsing the stored data, then for every item in the array, updating the stickyNoteContent variable which will be used to update the innerHTML below.
     let stickyNotesArray = JSON.parse(storedNotes);
     let stickyNoteContent = '';
-    //For each "sticky note" inside the array, we will display the below string with interpolated values for the text, date and time. Since the position of those values are constant within the array we can use their index to call them.
-    for (const note of stickyNotesArray) {
-    stickyNoteContent += 
-    `
-    <div class="stickyNoteDiv">
-    <i class="bi bi-x-square"></i>
-    <img src="/assets/stickyNote.png" class="stickyNoteImg" alt="Sticky Note">
-    <p class="stickyNoteText"> ${note[0]}</p>
-    <span class="stickyNoteDate">${note[1]} ${note[2]}</span>
-    </div>
-    `;
-    }
 
+    //For each "sticky note" inside the array, we will display the below string with interpolated values for the text, date and time. Since the position of those values are constant within the array we can use their index to call them.
+    for (let i = 0; i < stickyNotesArray.length; i++) {
+        const note = stickyNotesArray[i];
+        if (!note[2]) {
+            stickyNoteContent += 
+            `
+            <div class="stickyNoteDiv fadeInAnimation">
+            <i class="bi bi-x-square"></i>
+            <img src="/assets/stickyNote.png" class="stickyNoteImg" alt="Sticky Note">
+            <p class="stickyNoteText"> ${note[0]}</p>
+            <span class="stickyNoteDate">${note[1]}</span>
+            </div>
+            `;
+
+            // Update the boolean to indicate that the note has been displayed
+            stickyNotesArray[i][2] = true;
+        } else {
+            stickyNoteContent +=
+            `
+            <div class="stickyNoteDiv">
+            <i class="bi bi-x-square"></i>
+            <img src="/assets/stickyNote.png" class="stickyNoteImg" alt="Sticky Note">
+            <p class="stickyNoteText"> ${note[0]}</p>
+            <span class="stickyNoteDate">${note[1]}</span>
+            </div>
+            `;
+        }
+    }
     //Updating the inner HTML of the sticky note area section.
+    const stickyNoteArea = document.querySelector("#stickyNoteArea");
     stickyNoteArea.innerHTML = stickyNoteContent;
+
+    //Saving the changes to the boolean flag
+    let stickyNotesJSON = JSON.stringify(stickyNotesArray);
+    localStorage.setItem("stickyNotes", stickyNotesJSON);
 }
 
+// Add event listener for deleting notes by clicking the X, to the container using event delegation
+const stickyNoteContainer = document.querySelector("#stickyNoteArea");
+stickyNoteContainer.addEventListener("click", (event) => {if (event.target.matches("i")) deleteNote(event);});
 
-//# Creating the delete functionality
-//removeButtons represents all the buttons with the element i
-const removeButtons = document.getElementsByTagName("i");
 function deleteNote(event) {
-
-    //# Removing from display
-    //Variable we will use to store the DOM reference of the parent of the clicked i element
+    //# Removing from DOM
     let stickyNote = ''; 
     stickyNote = event.target.parentElement; 
-    //Setting a fade out animation
     stickyNote.style.animation = "fadeOutAnimation 0.5s forwards";
-    //Removing the note after 0.7 seconds
     setTimeout(() => stickyNote.remove(), 700)
-
+    
     //# Removing from localStorage
-    //By creating an array by getting the children of the parent node (the stickNoteArea section), we can virtually duplicate the array in storage in order to get the right array index for the sticky note we want to remove
-    let stickyNoteArea = stickyNote.parentElement;
-    const index = Array.from(stickyNoteArea.children).indexOf(stickyNote);
-
     //Loading the array from storage
     let storedNotes = localStorage.getItem("stickyNotes"); 
     let stickyNotesArray = JSON.parse(storedNotes);
-
+    
     //Splicing the selected item (sticky note) from the array
+    let stickyNoteArea = stickyNote.parentElement;
+    const index = Array.from(stickyNoteArea.children).indexOf(stickyNote);
     stickyNotesArray.splice(index, 1);
 
     //Saving the deletion
@@ -140,42 +140,3 @@ function deleteNote(event) {
     localStorage.setItem("stickyNotes", stickyNotesJSON);
 }
 
-// Add event listener to the container using event delegation
-const stickyNoteContainer = document.querySelector("#stickyNoteArea");
-stickyNoteContainer.addEventListener("click", function(event) {
-    // Check if the clicked element is a "remove" button, then removes the note
-    if (event.target.matches("i")) deleteNote(event);
-  });
-
-// Function to validate the date input
-function validateDateInput() {
-    const taskDeadlineDateBox = document.querySelector("#taskDeadlineDateBox");
-    const selectedDate = new Date(taskDeadlineDateBox.value).toLocaleDateString();
-    const minDate = new Date().toLocaleDateString();
-    if (selectedDate < minDate) {
-        taskDeadlineDateBox.setCustomValidity("Selected date is earlier than the minimum date.");
-    }
-}
-
-//Function to validate the hour input
-function validateHourInput() {
-    const taskDeadlineHourBox = document.querySelector("#taskDeadlineHourBox");
-    //Inputted time value. Converting into a number for more accurate comparison
-    let selectedHour = parseInt(taskDeadlineHourBox.value, 10)*100;
-    let selectedMinutes = parseInt(taskDeadlineHourBox.value.substring(3), 10);
-    let selectedTime = selectedHour + selectedMinutes;
-
-    //Creating a new date object of "now" and getting the hour and minutes
-    let date = new Date();
-    let hour = date.getHours()
-    let minutes = date.getMinutes()
-    //Converting the "now" time into decimal
-    let minHour = (hour*100) + minutes;
-
-    //If the time the user selects is less than the current time, then it shows an error message. Otherwise sets validity to true
-    if (selectedTime < minHour) {
-        taskDeadlineHourBox.setCustomValidity("Selected time is earlier than the minimum time.");
-    } else {
-        taskDeadlineHourBox.setCustomValidity('');
-    }
-}
